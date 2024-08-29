78bac84d65d2d1a9af5e6aa23946a64c74c90f3f
MazenJadaan <mazendev28@gmail.com>
2024-08-30 01:14:04 +0300

    Assignments for week 1

---
+++ Assignment for week 1/main.js
@@ -0,0 +1,40 @@
+//Assignment nom.2
+// document.write('<h1>Elzero</h1>')
+// document.querySelector('h1').style.color= 'blue';
+// document.querySelector('h1').style.fontSize= '80px';
+// document.querySelector('h1').style.textAlign= 'center';
+// document.querySelector('h1').style.fontFamily= 'arial';
+
+//Assignment nom.3
+// console.log('%cElzero %cWeb %cSchool','color:red; font-size:40px;','color:green; font-size:40px; font-wighet:bold','color:blue; font-size:40px')
+
+/*Assignment nom.4
+console.group("Group 1");
+console.log("Message One");
+console.log("Message Two");
+console.group("child group");
+console.log("Message One");
+console.log("Message Two");
+console.group("Grand child group");
+console.log("Message One");
+console.log("Message Two");
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+console.group("Group 2");
+console.log("Message One");
+console.log("Message Two");
+*/
+
+
+// Assignment nom.5
+// console.table(["Elzero","Ahmed","Sameh","Gamal","Aya"]);
+
+
+/* Assignment nom.6
+method one
+window.alert('error');
+metho two: comment the lines
+console.log("Iam In Console");
+document.write("Iam In Page");
+*/
